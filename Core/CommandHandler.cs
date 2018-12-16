20eb06b70cea754dbfd8c5b64d2f7864bb8c5e63
Leonel Jara <leoneljara3@gmail.com>
2018-12-15 19:28:30 -0500

    added core command handling, and discord api

---
+++ Core/CommandHandler.cs
@@ -0,0 +1,58 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace DiscordButlerBot.Core
+{
+    class CommandHandler {
+
+        DiscordSocketClient client_;
+        CommandService service_;
+        //position when the prefix end and the command begins
+        int argPos = 0;
+
+        public async Task InitAsyncTask(DiscordSocketClient client) {
+
+            client_ = client;
+            service_ = new CommandService();
+            //load the modules
+            await service_.AddModulesAsync(Assembly.GetEntryAssembly());
+            
+            client_.MessageReceived += HandleCommandAsync;
+
+        }
+
+        //Handling the recieving of a message 
+        private async Task HandleCommandAsync(SocketMessage s)
+        {
+            var userMsg = s as SocketUserMessage;
+
+            if (userMsg != null) {
+                Console.WriteLine("Messaged Recevied");
+                //info about the client
+                var context = new SocketCommandContext(client_, userMsg);
+
+                //If bot is mentioned
+                if (userMsg.HasStringPrefix(Config.bot.cmdPrefix, ref argPos) || userMsg.HasMentionPrefix(client_.CurrentUser, ref argPos))
+                {
+
+                    var result = await service_.ExecuteAsync(context, argPos);
+                    if (!result.IsSuccess && result.Error != CommandError.UnknownCommand)
+                    {
+                        Console.WriteLine("Problem HandleCommandAsync: " + result.ErrorReason);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Mentioned Received");
+                    }
+                }
+            }
+            
+        }
+    }
+}
--- Core/Program.cs
+++ Core/Program.cs
@@ -1,4 +1,7 @@
 ﻿using DiscordButlerBot.Core;
+using Discord.Commands;
+using Discord.WebSocket;
+using Discord;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +13,38 @@ namespace DiscordButlerBot
 {
     class Program
     {
+        public DiscordSocketClient client_;
+        public CommandHandler handler_;
+
         static void Main(string[] args)
+        => new Program().StartMainSync().GetAwaiter().GetResult();
+
+        public async Task StartMainSync() {
+
+            //Init
+            if (Config.bot.token != "" && Config.bot.token != null) {
+
+                //Init REST socket
+                client_ = new DiscordSocketClient(new DiscordSocketConfig { LogLevel = LogSeverity.Verbose });
+                client_.Log += Client_Logging;
+                
+                // Login/Start
+                await client_.LoginAsync(TokenType.Bot, Config.bot.token);
+                await client_.StartAsync();
+
+                //Init Command Handler
+                handler_ = new CommandHandler();
+                await handler_.InitAsyncTask(client_);
+                await Task.Delay(-1);
+            }
+            return;
+            
+        }
+
+        //Write to the console any log event
+        private async Task Client_Logging(LogMessage msg)
         {
-            Console.WriteLine("Test: " + Config.bot.token + "prefix: " + Config.bot.cmdPrefix);
-            Console.ReadLine();
+            Console.WriteLine("Client: " + msg.Message);
             
         }
     }
